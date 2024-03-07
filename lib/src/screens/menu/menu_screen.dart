31a9a03e70ce00d624a0061d1edf6b228b877b7e
ndyna <ndynagn05@gmail.com>
2024-03-08 05:22:55 +0600

    <+> Replace default lists to scrollable_positioned_list

--- lib/src/screens/menu/menu_screen.dart
+++ lib/src/screens/menu/menu_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_coffee_shop/src/common/extensions/context_extensions.dart';
-import 'package:flutter_coffee_shop/src/common/extensions/widget_extensions.dart';
 import 'package:flutter_coffee_shop/src/repo/menu/models.dart';
 import 'package:flutter_coffee_shop/src/repo/menu/repository.dart';
 import 'package:flutter_coffee_shop/src/screens/menu/widgets/category_tab.dart';
@@ -11,6 +10,7 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:gap/gap.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
 
 part 'menu_screen.g.dart';
 
@@ -31,44 +31,27 @@ class MenuScreen extends StatefulHookConsumerWidget {
 }
 
 class _MenuScreenState extends ConsumerState<MenuScreen> {
-  late final GlobalKey _listKey;
-  late final List<GlobalKey> _categoryGlobalKeys;
   late final Map<String, int> _selectedItems;
+  late final ItemPositionsListener _itemPositionsListener;
+  late final ItemScrollController _itemScrollController;
 
   @override
   void initState() {
-    _listKey = GlobalKey();
-    _categoryGlobalKeys = <GlobalKey>[];
     _selectedItems = <String, int>{};
+    _itemPositionsListener = ItemPositionsListener.create();
+    _itemScrollController = ItemScrollController();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     final categoriesData = ref.watch(categoriesProvider);
-    final scrollController = useScrollController();
     final selectedCategory = useState(0);
+    final itemPositionsListenable = _itemPositionsListener.itemPositions;
 
-    useEffect(
-      () {
-        if (_categoryGlobalKeys.isNotEmpty) {
-          WidgetsBinding.instance.addPostFrameCallback((_) {
-            final category = _categoryGlobalKeys[selectedCategory.value]
-                .currentContext
-                ?.findRenderObject();
-            if (category != null) {
-              scrollController.position.ensureVisible(
-                category,
-                duration: const Duration(milliseconds: 500),
-                curve: Curves.easeInOut,
-              );
-            }
-          });
-        }
-        return null;
-      },
-      [selectedCategory.value],
-    );
+    itemPositionsListenable.addListener(() {
+      selectedCategory.value = itemPositionsListenable.value.first.index;
+    });
 
     void onIncrement(String itemKey) {
       setState(() {
@@ -103,137 +86,121 @@ class _MenuScreenState extends ConsumerState<MenuScreen> {
 
     return RefreshIndicator(
       onRefresh: () => ref.refresh(categoriesProvider.future),
-      child: Scaffold(
-        body: categoriesData.when(
-          data: (data) {
-            if (_categoryGlobalKeys.isEmpty) {
-              _categoryGlobalKeys.addAll(
-                List.generate(
-                  data.length,
-                  (index) => GlobalKey(debugLabel: index.toString()),
-                ),
-              );
-              scrollController.addListener(() {
-                // centerItemIndex = getCenterItemIndex();
-              });
-            }
-            return CustomScrollView(
-              key: _listKey,
-              controller: scrollController,
-              slivers: [
-                SliverAppBar(
-                  systemOverlayStyle: const SystemUiOverlayStyle(
-                    systemNavigationBarColor: Colors.transparent,
-                    statusBarColor: Colors.transparent,
-                    statusBarIconBrightness: Brightness.dark,
-                  ),
-                  backgroundColor: Colors.transparent,
-                  bottom: PreferredSize(
-                    preferredSize: const Size.fromHeight(0),
-                    child: SizedBox(
-                      height: 40,
-                      child: ListView.separated(
-                        primary: false,
-                        shrinkWrap: true,
-                        scrollDirection: Axis.horizontal,
-                        padding: const EdgeInsets.symmetric(horizontal: 16),
-                        itemBuilder: (context, index) {
-                          return CategoryTab(
-                            tabIndex: index,
-                            selectedTabIndex: selectedCategory.value,
-                            category: data[index].category,
-                            onTap: () {
-                              selectedCategory.value =
-                                  data.indexOf(data[index]);
-                            },
+      child: categoriesData.when(
+        data: (data) {
+          return Scaffold(
+            appBar: AppBar(
+              systemOverlayStyle: const SystemUiOverlayStyle(
+                systemNavigationBarColor: Colors.transparent,
+                statusBarColor: Colors.transparent,
+                statusBarIconBrightness: Brightness.dark,
+              ),
+              backgroundColor: context.colorsExt.backgroundPrimaryColor,
+              elevation: 0,
+              scrolledUnderElevation: 0,
+              bottom: PreferredSize(
+                preferredSize: const Size.fromHeight(0),
+                child: SizedBox(
+                  height: 40,
+                  child: ListView.separated(
+                    primary: false,
+                    shrinkWrap: true,
+                    scrollDirection: Axis.horizontal,
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    itemBuilder: (context, index) {
+                      return CategoryTab(
+                        tabIndex: index,
+                        selectedTabIndex: selectedCategory.value,
+                        category: data[index].category,
+                        onTap: () {
+                          _itemScrollController.scrollTo(
+                            index: index,
+                            duration: const Duration(milliseconds: 400),
+                            curve: Curves.easeInOut,
                           );
                         },
-                        separatorBuilder: (context, index) {
-                          return const Gap(8);
-                        },
-                        itemCount: data.length,
-                      ),
-                    ),
+                      );
+                    },
+                    separatorBuilder: (context, index) {
+                      return const Gap(8);
+                    },
+                    itemCount: data.length,
                   ),
                 ),
-                const SliverGap(16),
-                SliverList.separated(
-                  itemCount: data.length,
-                  itemBuilder: (context, categoryIndex) {
-                    return Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          key: _categoryGlobalKeys[categoryIndex],
-                          data[categoryIndex].category,
-                          style: context.textExt.robotoSemiBold32,
-                        ),
-                        const Gap(16),
-                        MediaQuery.removePadding(
-                          context: context,
-                          removeTop: true,
-                          removeBottom: true,
-                          child: GridView.builder(
-                            primary: false,
-                            shrinkWrap: true,
-                            gridDelegate:
-                                SliverGridDelegateWithFixedCrossAxisCount(
-                              crossAxisCount: 2,
-                              mainAxisSpacing: 16,
-                              crossAxisSpacing: 16,
-                              childAspectRatio:
-                                  ((context.width - 24) / 2) / 230,
-                            ),
-                            itemCount: data[categoryIndex].items.length,
-                            itemBuilder: (context, index) {
-                              final model = data[categoryIndex].items[index];
-                              return MenuItem(
-                                model: model,
-                                onTap: () {},
-                                //TODO: Add functionality
-                                onDecrement: onDecrement,
-                                onIncrement: onIncrement,
-                                onPressed: addItem,
-                                count: _selectedItems[model.id],
-                              );
-                            },
-                          ),
-                        ),
-                      ],
-                    );
-                  },
-                  separatorBuilder: (context, index) {
-                    return const Gap(16);
-                  },
-                )
-                    .sliverPaddingSymmetric(horizontal: 16)
-                    .sliverSafeArea(top: false),
-                const SliverGap(16),
-              ],
-            );
-          },
-          error: (error, stackTrace) {
-            return SingleChildScrollView(
+              ),
+            ),
+            body: ScrollablePositionedList.builder(
+              itemCount: data.length,
+              itemPositionsListener: _itemPositionsListener,
+              itemScrollController: _itemScrollController,
+              padding: const EdgeInsets.all(16),
+              itemBuilder: (context, categoryIndex) {
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      data[categoryIndex].category,
+                      style: context.textExt.robotoSemiBold32,
+                    ),
+                    const Gap(16),
+                    GridView.builder(
+                      primary: false,
+                      shrinkWrap: true,
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        mainAxisSpacing: 16,
+                        crossAxisSpacing: 16,
+                        childAspectRatio: ((context.width - 24) / 2) / 240,
+                      ),
+                      itemCount: data[categoryIndex].items.length,
+                      itemBuilder: (context, index) {
+                        final model = data[categoryIndex].items[index];
+                        return MenuItem(
+                          model: model,
+                          onTap: () {},
+                          //TODO: Add functionality
+                          onDecrement: onDecrement,
+                          onIncrement: onIncrement,
+                          onPressed: addItem,
+                          count: _selectedItems[model.id],
+                        );
+                      },
+                    ),
+                  ],
+                );
+              },
+            ),
+          );
+        },
+        error: (error, stackTrace) {
+          return Material(
+            child: SingleChildScrollView(
               physics: const AlwaysScrollableScrollPhysics(),
               child: SizedBox(
                 height: context.height,
-                child: Center(
-                  child: Text('$error'),
+                child: SafeArea(
+                  child: Center(
+                    child: Text('$error'),
+                  ),
                 ),
               ),
-            );
-          },
-          loading: () {
-            return SingleChildScrollView(
+            ),
+          );
+        },
+        loading: () {
+          return Material(
+            child: SingleChildScrollView(
               child: SizedBox(
                 height: context.height,
-                child: const Center(
-                  child: CupertinoActivityIndicator(),
+                child: const SafeArea(
+                  child: Center(
+                    child: CupertinoActivityIndicator(),
+                  ),
                 ),
               ),
-            );
-          },
-        ),
+            ),
+          );
+        },
       ),
     );
   }
